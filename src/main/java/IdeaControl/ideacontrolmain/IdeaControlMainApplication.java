228300f32476456eea2c59311c6997e52391c74a
Guilherme Fonseca <79527437+Gui12128@users.noreply.github.com>
2024-02-16 15:21:10 -0300

    Initial commit

---
+++ src/main/java/IdeaControl/ideacontrolmain/IdeaControlMainApplication.java
@@ -0,0 +1,13 @@
+package IdeaControl.ideacontrolmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaControlMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaControlMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IdeaControl/ideacontrolmain/IdeaControlMainApplicationTests.java
@@ -0,0 +1,13 @@
+package IdeaControl.ideacontrolmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaControlMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
